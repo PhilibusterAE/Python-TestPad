21c61881f8f220cce7de9044ef3c2ba6ffc15d28
philipdain <32849288-philipdain@users.noreply.replit.com>
2024-03-16 09:08:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+students = int(input("How many students on the course? "))
+group_size_target = int(input("Desired group size? "))
+group_size = (students + group_size_target - 1) // group_size_target
+
+print(f"Number of groups formed: {group_size}")
